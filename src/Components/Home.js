5f72ad2817584b03324589269b3f14061d5fa8dd
Eshita95 <eshita.islam95@gmail.com>
2022-08-21 03:08:54 +0600

    live link added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Banner from './Banner';
 import Courses from './Courses';
 import Reviews from './Reviews';
-import Services from './Services';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <Services></Services>
             <Courses></Courses>
             <Reviews></Reviews>
         </div>
